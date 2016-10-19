f273ddb4e41f65b8b47589e1a4173ebaed5f6d1a
engmohannad <eng_mohalobaid@yahoo.com>
2016-10-19 23:59:49 +0200

    java sctipt demo

--- demo.js
+++ demo.js
@@ -5,6 +5,7 @@ function convertToF(celsius) {
    fahrenheit=((celsius*9)/5)+32;
   return fahrenheit;
 }
+
 var degree=prompt("Please enter Celsius temperture Degree");
 var result=convertToF(degree);
  console.log(result);
